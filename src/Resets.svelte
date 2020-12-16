59b8a284ac81bb3e5a62205426701176210d494e
Steven Ng <steven.ng@braintapper.com>
2020-12-15 20:08:39 -0500

    Case fix

---
+++ src/Resets.svelte
@@ -0,0 +1,67 @@
+<style global>
+* {
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+  text-align: left;
+  font-weight: inherit;
+  border-radius: 0px; }
+
+*:focus {
+  outline: none;
+  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px inset; }
+
+div {
+  position: relative; }
+
+button, input[type="button"], input {
+  border: none;
+  background: transparent;
+  font-family: var(--font-family-primary); }
+  button:focus, input[type="button"]:focus, input:focus {
+    outline: none; }
+
+a, a > em {
+  text-decoration: none; }
+
+a > em {
+  font-style: normal; }
+
+a:hover > em {
+  text-decoration: underline; }
+
+a, a:hover, a:visited, a:active {
+  color: var(--black); }
+
+html, body {
+  min-width: var(--breakpoint-min);
+  width: 100%;
+  height: 100%; }
+
+ul, ol {
+  list-style-type: none; }
+
+ul[unstyled], ol[unstyled] {
+  list-style-type: none !important; }
+  ul[unstyled] li, ol[unstyled] li {
+    margin-left: 0px !important; }
+
+table {
+  border-spacing: 0;
+  border-collapse: collapse; }
+  table td, table th {
+    vertical-align: top; }
+
+details summary::-webkit-details-marker {
+  display: none;
+  font-size: 0px; }
+
+a, b, i, strong, em {
+  font-weight: inherit;
+  text-decoration: inherit; }
+
+hr {
+  border-top: 1px solid var(--lightest-gray); }
+
+
+</style>
