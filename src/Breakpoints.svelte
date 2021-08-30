240c22f081a6512a6ed0635b835b895f76fc327e
Steven Ng <steven.ng@braintapper.com>
2021-08-30 17:16:39 -0400

    Add responsive breakpoint global vars, ver bump

---
+++ src/Breakpoints.svelte
@@ -0,0 +1,19 @@
+<style>
+
+  :global(body) {
+
+    --screen-xs: 0px;
+    --screen-xs-max: 599px;
+    --screen-sm: 600px;
+    --screen-sm-max: 959px;
+    --screen-md: 960px;
+    --screen-md-max: 1259px;
+    --screen-lg: 1280px;
+    --screen-lg-max: 1919px;
+    --screen-xl: 1920px;
+    --screen-xl-max: 2559px;
+    --screen-xxl: 2560px;
+    
+  }
+
+</style>
